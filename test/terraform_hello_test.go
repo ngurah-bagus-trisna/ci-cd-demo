40d382fe0c180a505708f9eb42eb705edb4f108b
ngurah-bagus-trisna <ngurahbagusnb391@gmail.com>
2025-06-17 11:05:14 +0700

    ci: Create infrasturcture as a code testing before script can apply to main branch
    Skenario when some contributors add changes on iaac code, and before merge, script need test.

---
+++ test/terraform_hello_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHelloFile(t *testing.T) {
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+	
+	content, err := ioutil.ReadFile("hello.txt")
+	assert.NoError(t, err)
+	assert.contains(t, string(content), "Hello, OpenTofu!")
+}	
